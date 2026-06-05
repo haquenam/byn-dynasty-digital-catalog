757224c961124ef48049fed44da6445bda795013
Enamul Haque <enaminspire@gmail.com>
2026-06-05 11:43:06 +0100

    Update catalog thumbnails

---
+++ catalog-data.js
@@ -0,0 +1,20 @@
+const catalogData = [
+  {
+    id: 'menswear',
+    title: 'Menswear',
+    thumbnailPath: 'assets/logo.png',
+    pdfPath: 'assets/pdfs/menswear-may-2026.pdf',
+  },
+  {
+    id: 'womenswear',
+    title: 'Womenswear',
+    thumbnailPath: 'assets/logo.png',
+    pdfPath: 'assets/pdfs/womenswear-may-2026.pdf',
+  },
+  {
+    id: 'kidswear',
+    title: 'Kidswear',
+    thumbnailPath: 'assets/kidswear-flyer.png',
+    pdfPath: 'assets/pdfs/kidswear-may-2026.pdf',
+  },
+];
